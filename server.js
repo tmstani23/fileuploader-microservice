dc16295612a9db0efc8b47d3620a004c58106400
Tim Work Laptop <tmstani23@gmail.com>
2019-06-03 15:42:53 -0500

    Update 1
    Added new project and skeleton files.  Began work on file uploader post controller.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var express = require('express');
+var multer  = require('multer')
+var upload = multer({ dest: '/api/fileanalyse' })
 var cors = require('cors');
 
 // require and use "multer"...
@@ -17,6 +19,17 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  // req.file is the `avatar` file
+  //console.log(req.body);
+  //console.log(req.file);
+  // req.body will hold the text fields, if there were any
+  res.json({
+    filename: req.file.originalname,
+    file_size: req.file.size
+  });
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
